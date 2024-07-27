c714f1be8d8aebca5b534b542cc2a226a5e2ffac
chinhsientsai <tsai.ch@msa.hinet.net>
2024-07-27 16:27:26 +0800

    modify

---
+++ 2024_07_27/lesson07_27_4.py
@@ -0,0 +1,49 @@
+import os.path
+import os
+from datetime import datetime
+import random
+
+current_path = os.path.abspath(__name__)
+directory_name = os.path.dirname(current_path)
+data_path = os.path.join(directory_name,'data')
+print(data_path)
+if not os.path.isdir(data_path):
+    print("沒有data的目錄,手動建立目錄")
+    os.mkdir(data_path)
+else:
+    print("目錄已建立")
+
+log_path = os.path.join(data_path, 'iot.log')
+if not os.path.isfile(log_path):
+    print("沒有iot.log檔,建立新檔")
+    with open(log_path,mode='w',encoding='utf-8',newline='') as file:
+        file.write('時間,濕度,溫度\n')
+    
+else:
+    print("已經有log檔")
+
+current_path = os.path.abspath(__name__)
+directory_name = os.path.dirname(current_path)
+data_path = os.path.join(directory_name,'data')
+print(data_path)
+if not os.path.isdir(data_path):
+    print("沒有data的目錄,手動建立目錄")
+    os.mkdir(data_path)
+else:
+    print("目錄已建立")
+
+log_path = os.path.join(data_path, 'iot.log')
+if not os.path.isfile(log_path):
+    print("沒有iot.log檔,建立新檔")
+    with open(log_path,mode='w',encoding='utf-8',newline='') as file:
+        file.write('時間,濕度,溫度\n')
+    
+else:
+    print("已經有log檔")
+
+now = datetime.now()
+now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+humidity = str(random.randint(330,820) / 10)
+celsius = str(random.randint(50,400) / 10)
+with open(log_path,mode='a',encoding='utf-8',newline='') as file:
+    file.write(now_str + ',' + humidity + ',' + celsius + "\n")
